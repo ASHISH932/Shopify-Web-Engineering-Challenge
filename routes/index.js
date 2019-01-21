b856a28f5a00cebfdc46823ea001b4223c979b69
Ashish <sharma.ashish960@gmail.com>
2019-01-21 14:36:39 +0530

    In-memory cache added to store most frequent searches

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,22 @@ const router = express.Router()
 
 const { getWastes } = require('../util')
 
-router.get('/wastes', async (req, res) => {
-    let keyword = req.query.keyword, wastes = await getWastes()
+// router.get('/wastes', async (req, res) => {
+exports.getWaste = async (req,res, cache) => {
+    let keyword = req.query.keyword;
+    const x = keyword+'_o';
+
+    // Increase the occurence count of keyword
+    const occurence = cache.get(x) !== null ? cache.get(x) + 1 : 1;
+    cache.put(x, occurence);
+
+    // Check if keyword is saved in cache
+    if(cache.get(keyword) !== null){
+        console.log('Served from cache');
+        res.send(cache.get(keyword));
+        return;
+    }
+    let wastes = await getWastes()
     
     if (!_.isEmpty(keyword)) {
         keyword = keyword.trim().toLowerCase()
@@ -13,7 +27,17 @@ router.get('/wastes', async (req, res) => {
         wastes.forEach((w,i) => w.id= i);
         wastes = wastes.filter(w => w.keywords.trim().toLowerCase().includes(keyword))
     }
+    if(occurence > 5){
+        // Cache the keyword for 6 hours if occurence is greater than 5
+        cache.put(req.query.keyword, wastes, 21600000, (key, value) => {
+            /* Reduce the value of occurence by 5 this would help to 
+            remove item from frequently used list which are no more frequently used 
+            */
+            let occurence = cache.get(x) !== null ? cache.get(x) -5 : 0;
+            cache.put(x,occurence);
+        })
+    }
     res.send(wastes)
-})
+}
 
-module.exports = router
+// module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const router = express.Router();
+const cache = require('memory-cache');
 
 const routes = require('./routes');
 const app = express();
@@ -11,7 +13,8 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api', routes);
+app.use('/api', router);
+router.get("/wastes", (req,res) => routes.getWaste(req,res,cache));
 
 app.use(express.static(publicPath));
 
